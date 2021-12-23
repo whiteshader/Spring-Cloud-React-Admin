38c4a85c1ec3e170aaaf3249b182580dc9215339
whiteshader <whiteshader@163.com>
2021-12-23 13:55:32 +0800

    change page num

--- ruoyi-common/src/main/java/com/ruoyi/common/core/page/TableSupport.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/page/TableSupport.java
@@ -12,7 +12,7 @@ public class TableSupport
     /**
      * 当前记录起始索引
      */
-    public static final String PAGE_NUM = "pageNum";
+    public static final String PAGE_NUM = "current";
 
     /**
      * 每页显示记录数
