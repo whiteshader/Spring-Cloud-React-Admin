96bbfab157cb2c4e929c95ae4634df9e80be70c4
whiteshader <whiteshader@163.com>
2022-02-14 15:04:00 +0800

    add icon selector

--- react-ui/config/config.ts
+++ react-ui/config/config.ts
@@ -21,7 +21,6 @@ export default defineConfig({
   },
   // https://umijs.org/zh-CN/plugins/plugin-locale
   locale: {
-    // default zh-CN
     default: 'zh-CN',
     antd: true,
     // default true, when it is true, will use `navigator.language` overwrite default
