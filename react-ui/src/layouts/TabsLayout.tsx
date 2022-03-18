58957b025cf8534d7d82aea1772aba1b8e6470a5
whiteshader <whiteshader@163.com>
2022-03-18 14:54:45 +0800

    mix layout style

--- react-ui/src/layouts/TabsLayout.tsx
+++ react-ui/src/layouts/TabsLayout.tsx
@@ -4,15 +4,14 @@ import KeepAliveTabs from '@/components/KeepAliveTabs';
  *
  * @author whiteshader@163.com
  * @datetime  2022/02/22
- * 
+ *
  * */
 
 const TabsLayout: React.FC = (props) => {
-
   return (
     <div>
       <KeepAliveTabs />
-      {props.children}
+      <div style={{ margin: '24px' }}>{props.children}</div>
     </div>
   );
 };
